6dcc0b100df513c5ab42651bc63b1aa0a36f0c7e
HikmalRP <147077994+HikmalRP@users.noreply.github.com>
2024-03-13 13:37:06 +0700

    Add files via upload

---
+++ latihan_2c.java
@@ -0,0 +1,15 @@
+package minggu_1;
+
+public class latihan_2c {
+
+	public static void main(String[] args) {
+		System.out.println("Menghitung volume tabung dengan diameter = 5 dan tinggi = 10");
+		int a = 5;
+		int b = 10;
+		double volume_tabung;
+		volume_tabung = Math.PI*a/2*a/2*b;
+		System.out.println("Hasil volume tabung dengan diameter = 5 dan tinggi = 10 adalah " + volume_tabung);
+
+	}
+
+}
