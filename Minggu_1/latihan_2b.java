b02f69ee0c6b9bcbba3c22eba995761afb4d9150
HikmalRP <147077994+HikmalRP@users.noreply.github.com>
2024-03-13 13:41:46 +0700

    Add files via upload

---
+++ Minggu_1/latihan_2b.java
@@ -0,0 +1,15 @@
+package minggu_1;
+
+public class latihan_2b {
+
+	public static void main(String[] args) {
+		System.out.println("Menghitung luas segitiga siku-siku dengan alas = 6 dan tinggi = 10");
+		int a = 6;
+		int b = 10;
+		double luas_segitiga;
+		luas_segitiga = (a*b)/2;
+		System.out.println("Hasil luas segitiga siku-siku dengan alas = 6 dan tinggi = 10 adalah " + luas_segitiga);
+
+	}
+	
+}
